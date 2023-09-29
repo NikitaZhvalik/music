728967387628308583ddc0b4d96ee6693209b7fc
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-09-29 15:42:44 +1000

    05: реализация бургер-меню

--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -1,31 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./style.css"
 import menuItem from "./const.js"
 
 const NavMenu = () => {
+
+    const [show, setShow] = useState(true)
+    const toggleShow = () => setShow(!show)
+
     return (
         <div>
           <nav className="main__nav nav">
             <div className="nav__logo logo">
               <img className="logo__image" src="/img/logo.png" alt="logo" />
             </div>
-            <div className="nav__burger burger">
+            <div onClick={toggleShow} className="nav__burger burger">
               <span className="burger__line"></span>
               <span className="burger__line"></span>
               <span className="burger__line"></span>
             </div>
-            <div className="nav__menu menu">
-              <ul className="menu__list">
-              {menuItem.map((item) => {
-                return (
-                  <li key={item.name} className="menu__item">
-                    <a href={item.link} className="menu__link">{item.name}</a>
-                  </li>
-                )
-              })}
-              </ul>
-            </div>
+            {
+              show ?             
+                    <div className="nav__menu menu">
+                    <ul className="menu__list">
+                    {menuItem.map((item) => {
+                      return (
+                        <li key={item.name} className="menu__item">
+                          <a href={item.link} className="menu__link">{item.name}</a>
+                        </li>
+                      )
+                    })}
+                    </ul>
+                  </div> 
+              : ''
+            }
           </nav>
         </div>
     )
